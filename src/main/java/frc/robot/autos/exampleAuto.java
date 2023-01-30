b72a1bca2403652e39aa95da52cfbf5708ae4c01
CooperWang0912 <90701697+CooperWang0912@users.noreply.github.com>
2023-01-30 13:07:08 -0800

    Update exampleAuto.java

--- src/main/java/frc/robot/autos/exampleAuto.java
+++ src/main/java/frc/robot/autos/exampleAuto.java
@@ -32,9 +32,9 @@ public class exampleAuto extends SequentialCommandGroup {
                 // Start at the origin facing the +X direction
                 new Pose2d(0, 0, new Rotation2d(0)),
                 // Pass through these two interior waypoints, making an 's' curve path
-                List.of(new Translation2d(0 * coefficient, -0.5 * coefficient)),
+                List.of(new Translation2d(0.5 * coefficient, 0 * coefficient)),
                 // End 3 meters straight ahead of where we started, facing forward
-                new Pose2d(0 * coefficient, 0 * coefficient, new Rotation2d(2)),
+                new Pose2d(1 * coefficient, 0 * coefficient, new Rotation2d(0)),
                 config);
 
         var thetaController =
