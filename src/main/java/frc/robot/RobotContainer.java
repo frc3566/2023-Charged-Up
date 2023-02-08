f16e911a945045e6ea3e50ae108b9d2bf89dcd78
Kaien <kaienhung999@gmail.com>
2023-02-07 19:56:20 -0500

    Add telescopingWench + boomWench dpad

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 
 import java.io.IOException;
 
@@ -34,17 +35,17 @@ public class RobotContainer {
     public static double speedCoefficient = 1.0;
 
     /* Driver Buttons */
-    private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kX.value);
+    // private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kX.value);
 
-    private final JoystickButton armDown = new JoystickButton(driver, XboxController.Button.kB.value);
+    private final POVButton boomWenchUp = new POVButton(driver, 0);
+    private final POVButton boomWenchDown = new POVButton(driver, 180);
+    private final POVButton telescopingWenchIn = new POVButton(driver, 90);
+    private final POVButton telescopingWenchOut = new POVButton(driver, 270);
 
     private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kA.value);
     private final JoystickButton increaseSpeed = new JoystickButton(driver, XboxController.Button.kRightBumper.value);
     private final JoystickButton decreaseSpeed = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
     private final JoystickButton runTrajectory = new JoystickButton(driver, XboxController.Button.kY.value);
-
-    /* Trajectory */
-    private static Trajectory teleopTrajectory = null;
 
     /* Subsystems */
     private final Swerve s_Swerve = new Swerve();
@@ -78,9 +79,19 @@ public class RobotContainer {
      */
     private void configureButtonBindings() {
         /* Driver Buttons */
-        zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
+        // zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
 
-        armDown.onTrue(new InstantCommand(() -> arm.run()));
+        boomWenchUp.onTrue(new InstantCommand(() -> arm.boomWenchUp()));
+        boomWenchUp.onFalse(new InstantCommand(() -> arm.boomWenchOff()));
+
+        boomWenchDown.onTrue(new InstantCommand(() -> arm.boomWenchDown()));
+        boomWenchDown.onFalse(new InstantCommand(() -> arm.boomWenchOff()));
+
+        telescopingWenchOut.onTrue(new InstantCommand(() -> arm.telescopingWenchOut()));
+        telescopingWenchOut.onFalse(new InstantCommand(() -> arm.telescopingWenchOff()));
+
+        telescopingWenchIn.onTrue(new InstantCommand(() -> arm.telescopingWenchIn()));
+        telescopingWenchIn.onFalse(new InstantCommand(() -> arm.telescopingWenchOff()));
 
         increaseSpeed.onTrue(new InstantCommand(() -> s_Swerve.increaseSpeed()));
         decreaseSpeed.onTrue(new InstantCommand(() -> s_Swerve.decreaseSpeed()));
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,26 +4,46 @@ import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.MotorCommutation;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Arm extends SubsystemBase {
     private CANSparkMax boomWench;
     private CANSparkMax telescopingWench;
-    private boolean on = false;
+    private boolean onB = false;
+    private boolean onT = false;
 
     public Arm() {
         boomWench = new CANSparkMax(Constants.ArmConstants.boomWenchID, MotorType.kBrushless);
+        telescopingWench = new CANSparkMax(Constants.ArmConstants.telescopingWenchID, MotorType.kBrushless);
     }
 
     public void run() {
-        if (on) {
-            boomWench.stopMotor();
-            on = false;
-        } else {
-            boomWench.set(0.2);
-            on = true;
-        }
     }
 
+    public void boomWenchUp() {
+        boomWench.set(0.2);
+    }
+
+    public void boomWenchDown() {
+        boomWench.set(-0.2);
+    }
+
+    public void boomWenchOff() {
+        boomWench.stopMotor();
+    }
+
+    public void telescopingWenchOut() {
+        telescopingWench.set(-0.2);
+    }
+    public void telescopingWenchIn() {
+        telescopingWench.set(0.2);
+    }
+
+    public void telescopingWenchOff() {
+        telescopingWench.stopMotor();
+    }
 }
