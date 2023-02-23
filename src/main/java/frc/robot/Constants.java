e1add56cb1ba6ead1b07187fae479ecdc09e99e7
Kaien <kaienhung999@gmail.com>
2023-02-23 15:47:10 -0500

    Update intakeId constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -74,7 +74,7 @@ public final class Constants {
     }
 
     public static class IntakeConstants{
-        public static final int intakeID = 0;
+        public static final int intakeID = 21;
     }
 
     /* LENGTH > WIDTH */
