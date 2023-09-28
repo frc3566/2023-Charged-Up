e998a36a72186e0857dc732f03e0597a7e452424
GregoryLi360 <gregoryli360@gmail.com>
2023-09-28 16:12:02 -0400

    prepare vision for testing rotation

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -69,11 +69,9 @@ public class Vision extends SubsystemBase {
 
         System.out.println("yaw: " + target.getYaw());
         System.out.println("range: " + dist);
-        System.out.println("camera to target translation/transform: " + target.getBestCameraToTarget().getTranslation() + "\n");
+        System.out.println("target: " + target);
 
         return Optional.of(target.getBestCameraToTarget());
-    
-
     }
 
     public Optional<Trajectory> getTrajectory() {
@@ -90,13 +88,12 @@ public class Vision extends SubsystemBase {
 
         /* Pose2d start, List<Translation2D> pathPoints, Pose2d end, config */
         // TODO: Fix Rotation2d of the end pos
-        return Optional.of(TrajectoryGenerator.generateTrajectory(
-            new Pose2d(0, 0, new Rotation2d(0)),
-            List.of(end.div(2)),
-            new Pose2d(end, new Rotation2d()),
-            config
-        ));
-
-        
+        return Optional.empty();
+        // return Optional.of(TrajectoryGenerator.generateTrajectory(
+        //     new Pose2d(0, 0, new Rotation2d(0)),
+        //     List.of(end.div(2)),
+        //     new Pose2d(end, new Rotation2d()),
+        //     config
+        // ));
     }
 }
